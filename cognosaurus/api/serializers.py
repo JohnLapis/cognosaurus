550249053a184754b40cf8c9d40f2fa0191fc4b4
john <borges-2014@live.com>
2021-02-01 20:28:57 -0300

    squash! feat: Add serializer for cognates

--- cognosaurus/api/serializers.py
+++ cognosaurus/api/serializers.py
@@ -2,9 +2,5 @@ from rest_framework import serializers
 
 
 class CognateSerializer(serializers.Serializer):
-    # make sure it is unicode and verify if 30 is a good limit
-    # how to account for whitespace and punctuation?
-    # how to make it international too?
-    # should i use a regex??
-    cognate = serializers.CharField(max_length=30)
-    language = serializers.CharField(max_length=30)
+    cognate = serializers.CharField(max_length=80)
+    language = serializers.CharField(max_length=60)
