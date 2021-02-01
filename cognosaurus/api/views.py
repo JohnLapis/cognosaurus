5e05eee89c1d50980cb8ab030a74f05f1f2ccbf1
john <borges-2014@live.com>
2021-02-01 11:32:36 -0300

    feat: Add CognateViewSet

--- cognosaurus/api/views.py
+++ cognosaurus/api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
 
-# Create your views here.
+from cognosaurus.api.serializers import CognateSerializer
+
+LANGUAGES = ["en", "int", "jp", "pt-br", "ru"]
+
+
+class CognateViewSet(ViewSet):
+    def get_queryset(self, word, lang):
+        return [{"cognate": word, "language": lang}]
+
+    def list(self, request):
+        results = {}
+        for lang, words in request.query_params.lists():
+            for word in words:
+                if lang not in LANGUAGES and False:
+                    continue
+                queryset = self.get_queryset(word, lang)
+                serializer = CognateSerializer(data=queryset, many=True)
+                assert serializer.is_valid()
+                results[word] = serializer.data
+
+        return Response({"results": results})
--- cognosaurus/urls.py
+++ cognosaurus/urls.py
@@ -1,4 +1,11 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from cognosaurus.api.views import CognateViewSet
 
 
-urlpatterns = []
+router = routers.DefaultRouter()
+router.register(r"words", CognateViewSet, basename="word")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
