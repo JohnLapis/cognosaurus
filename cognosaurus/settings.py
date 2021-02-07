a9928185c6e2da9ac2ed83c2709efa9a2d53f948
john <borges-2014@live.com>
2021-02-07 14:05:25 -0300

    chore: Add 127.0.0.1 and 0.0.0.0 to ALLOWED_HOSTS

--- cognosaurus/settings.py
+++ cognosaurus/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0"]
 
 INSTALLED_APPS = [
     "drf_spectacular",
