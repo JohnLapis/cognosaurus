f88f26254a6381763262417019d45625ed05404d
john <borges-2014@live.com>
2021-02-10 21:24:49 -0300

    chore: Python versions in tests can be altered by environment variables

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -10,8 +10,14 @@ echo "Initiating redis"
 redis-server --port $REDIS_PORT --daemonize yes
 cd ..
 
+if [ -z ${PYTHON_VERSION+x} ]; then
+    envlist=ALL
+else
+    envlist=$(echo $PYTHON_VERSION | sed -E 's/([0-9])\./py\1/')
+fi
+
 echo "Running tox"
-tox -- cognosaurus/api/tests/integration/ "$@"
+tox -e $envlist -- cognosaurus/api/tests/integration/ "$@"
 
 echo "Terminating redis"
 redis-cli -p $REDIS_PORT shutdown nosave
